b5b6fb80836f3b82f4956a1df83a04ff8af28d99
Vinicius Fernandes <vinicius.turu@gmail.com>
2022-06-09 08:34:45 -0300

    ajuste no cadastro dos professores

--- cad_prof.php
+++ cad_prof.php
@@ -10,16 +10,18 @@ if(!isset($_SESSION['admin_name'])){
 $iniciado;
 
 if(isset($_POST['submit'])){
-    $nome_material = $_POST['nome'];
+    $nome = $_POST['nome'];
     $image = $_POST['image'];
+    $cargo = $_POST['cargo'];
     $desc = $_POST['descricao'];
+    $link = $_POST['link'];
     
     $select = " SELECT * FROM professores";
 
     $result = mysqli_query($conn, $select);
 
     if(mysqli_num_rows($result) >= 0){
-        $insert = "INSERT INTO professores(nome, img, descrição) VALUES('$nome_material', '$image', '$desc')";
+        $insert = "INSERT INTO professores(nome, img, cargo, descrição, link) VALUES('$nome', '$image', '$cargo', '$desc', '$link')";
         mysqli_query($conn, $insert);
         $correct[] = 'Professor cadastrado!';
     }
@@ -61,8 +63,12 @@ if(isset($_POST['submit'])){
             <input required type="text" id="nome" name="nome" placeholder="Ex: João Silva">
             <label for="image">Foto do professor</label>
             <input required type="text" name="image" id="image" placeholder="Ex: http://www.example.com/images/...jpg">
+            <label for="cargo">Cargo do Professor</label>
+            <input required type="text" name="cargo" id="cargo" placeholder="Ex: Palestrante">
             <label for="descricao">Descrição do Professor</label>
-            <textarea required name="descricao" id="descricao" placeholder="Limite máximo: 100 caracteres." rows="4" Maxlength="100"></textarea>
+            <textarea required name="descricao" id="descricao" placeholder="Para pular linha utilize a tag <br> 2 vezes." rows="4" Maxlength="9000"></textarea>
+            <label for="link">Link (opcional)</label>
+            <input type="text" name="link" id="link" placeholder="Ex: http://site-do-palestrante.com">
             <input type="submit" name="submit" value="submit">
         </form>
     </div>
